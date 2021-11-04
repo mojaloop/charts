aab96886fab2f7e831b770584b80eb942922effc
Vijay Kumar <vijaya.guthi@modusbox.com>
2021-11-04 15:28:36 +0530

    Add bof charts to script files

--- update-charts-dep.sh
+++ update-charts-dep.sh
@@ -18,6 +18,16 @@ declare -a charts=(
         # Common Charts
         mojaloop/common
         # Mojaloop BoF Charts
+        mojaloop/security-role-perm-crd
+        mojaloop/role-assignment-service
+        mojaloop/reporting-hub-bop-shell
+        mojaloop/reporting-hub-bop-api-svc
+        mojaloop/reporting-events-processor-svc
+        mojaloop/security-role-perm-operator-svc
+        mojaloop/reporting-hub-bop-role-ui
+        mojaloop/reporting-hub-bop-trx-ui
+        mojaloop/security-hub-bop-kratos-ui
+        mojaloop/bof
         ## placeholder
         # Mojaloop Core Charts
         mojaloop/admin-api-svc
