db46c23c6ff0dd02984531f7f5e2e54055e48dec
Lewis Daly <lewis@vesselstech.com>
2021-11-15 11:34:42 +0930

    chore: cleanup unused code

--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ `echo 1` = "1" ]] ; then 
-  printf "    helm install of ingress-nginx sucessful after <$elapsed_secs> secs \n\n"
-else 
-  printf "    Error: ingress-nginx helm chart  deployment failed "
-  exit 1
-fi 
