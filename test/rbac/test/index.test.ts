f16f50ff92e3a825d0a7f804d0acd9b2cde1c805
msk- <mattkingston@gmail.com>
2022-01-12 15:46:53 +0000

    WIP

--- test/rbac/test/index.test.ts
+++ test/rbac/test/index.test.ts
@@ -1,6 +1,31 @@
+import got, { OptionsOfJSONResponseBody } from 'got';
+import { components } from 'types/role_assignment';
+
+type Users = components["schemas"]["UsersGetResponse"];
+type User = components["schemas"]["User"];
+type Role = string;
+
+
+interface TestParameters {
+    roles: Role[];
+
+}
+
+const basePath = 'http://localhost:3008';
+const testUserName = 'test1';
 
 describe('nothing', () => {
+    let testUserId: User["id"];
+    beforeAll(async () => {
+        const response = await got.get<Users>(`${basePath}/users`);
+        expect(response.statusCode).toEqual(200);
+        console.log(response.body);
+        const user = response.body.users?.find((user) => user.name === testUserName);
+        expect(user?.id).toBeDefined();
+        testUserId = user!.id;
+    });
     test('whatever', () => {
+        console.log(testUserId);
         expect(true);
     });
 });
