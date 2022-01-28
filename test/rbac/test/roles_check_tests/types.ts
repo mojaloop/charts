1013372b40a250022cf850475857d54826b9fb0c
Vijay Kumar <vijaya.guthi@modusbox.com>
2022-01-28 23:26:14 +0530

    Refactored

--- test/rbac/test/roles_check_tests/types.ts
+++ test/rbac/test/roles_check_tests/types.ts
@@ -29,24 +29,17 @@
  --------------
  ******/
 
-import * as env from 'env-var';
+import { components } from '../types/role_assignment';
+import { Method } from 'got';
 
-export const roleAssignmentSvcBasePath = env.get('ROLE_ASSIGNMENT_SVC_BASE_PATH')
-    .default('http://localhost:3008')
-    .asUrlObject();
+export type Users = components["schemas"]["UsersGetResponse"];
+export type User = components["schemas"]["User"];
+export type Roles = components["schemas"]["UsersIDRolesGetResponse"];
+export type Role = Roles["roles"][0];
+export type RolePatch = components["schemas"]["UsersIDRolesPatchRequest"];
 
-// It's useful to parse this as a URL object for validation. But any query strings will be
-// discarded, only the origin and path will be used.
-const basePathUrl = env.get('ML_INGRESS_BASE_PATH')
-    .default('http://bofportal.test.infra.mojatest.live.internal')
-    .asUrlObject();
-export const mlIngressBasePath = `${basePathUrl.origin}${basePathUrl.pathname}`.replace(/\/*$/, '');
-export const proxyPrefix = 'proxy';
-
-export const username = env.get('TEST_USER_NAME')
-    .default('test1')
-    .asString();
-
-export const password = env.get('TEST_USER_PASSWORD')
-    .default('wGdfOKOdi7TTc0UmTorpSLMYiOjgPZ')
-    .asString();
+export interface TestParameters {
+  role: Role;
+  url: URL;
+  method: Method;
+}
